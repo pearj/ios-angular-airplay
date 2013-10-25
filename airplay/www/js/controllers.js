96cdfb4c1d260b86a9c48d2c99131e7d9b3ccb08
japearson <japearson@agiledigital.com.au>
2013-10-25 15:34:18 +1100

    Switch to angular

---
+++ airplay/www/js/controllers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', '$location',
+  function($scope, Phone, $location) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+    $scope.goToSecondScreen = function(url) {
+       if (typeof PGExternalScreen === 'undefined') {
+          window.location = '#/phones/' + url;
+       }
+       else {
+          PGExternalScreen.loadHTMLResource('index.html#/phones/' + url);
+       }
+       console.log('index.html#/phones/' + url);
+    }
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
